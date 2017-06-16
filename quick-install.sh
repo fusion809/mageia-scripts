0b9ed8044b3c3eab48a2925ac124fe0fd6a66edf
fusion809 <fusion809@users.noreply.github.com>
2017-06-16 15:53:49 +1000

    Updating quick-install to make it fit my modern conventions

--- quick-install.sh
+++ quick-install.sh
@@ -1,63 +1,65 @@
 # Create GitHub directory
-if ! [[ -d $HOME/GitHub ]]; then
-  mkdir $HOME/GitHub
+if ! [[ -d $HOME/GitHub/mine/scripts ]]; then
+    mkdir $HOME/GitHub/mine/scripts
 fi
 
 # Get openssh, if not pre-installed and Zsh
 if ! `which ssh >/dev/null 2>&1`; then
-  sudo urpmi openssh
+    sudo urpmi openssh
 fi
 
 if ! [[ -f /bin/zsh ]]; then
-  sudo urpmi zsh
+    sudo urpmi zsh
 fi
 
 if ! [[ -f /usr/bin/git ]]; then
-  sudo urpmi git
+    sudo urpmi git
 fi
 
 # Clone mageia-scripts repo
-if ! [[ -d $HOME/GitHub/mine/mageia-scripts ]]; then
-  git clone https://github.com/fusion809/mageia-scripts $HOME/GitHub/mine/mageia-scripts
-  # Copy across
-  cp -a $HOME/GitHub/mine/mageia-scripts/{Shell,.bashrc,.zshrc} $HOME/
-  sudo cp -a $HOME/GitHub/mine/mageia-scripts/root/{Shell,.bashrc,.zshrc} /root/
+if ! [[ -d $HOME/GitHub/mine/scripts/mageia-scripts ]]; then
+    git clone https://github.com/fusion809/mageia-scripts $HOME/GitHub/mine/scripts/mageia-scripts
+
+    # Copy across
+    cp -a $HOME/GitHub/mine/scripts/mageia-scripts/{Shell,.bashrc,.zshrc} $HOME/
+    sudo cp -a $HOME/GitHub/mine/scripts/mageia-scripts/root/{Shell,.bashrc,.zshrc} /root/
 else
-  cd $HOME/GitHub/mine/mageia-scripts
-  git pull origin master
-  cd -
-  # Copy across
-  cp -a $HOME/GitHub/mine/mageia-scripts/{Shell,.bashrc,.zshrc} $HOME/
-  sudo cp -a $HOME/GitHub/mine/mageia-scripts/root/{Shell,.bashrc,.zshrc} /root/
+    pushd $HOME/GitHub/mine/scripts/mageia-scripts
+    git pull origin master
+    popd -
+
+    # Copy across
+    cp -a $HOME/GitHub/mine/scripts/mageia-scripts/{Shell,.bashrc,.zshrc} $HOME/
+    sudo cp -a $HOME/GitHub/mine/scripts/mageia-scripts/root/{Shell,.bashrc,.zshrc} /root/
 fi
 
 if ! [[ -d $HOME/.oh-my-zsh ]]; then
-  # git clone oh-my-zsh
-  git clone https://github.com/robbyrussell/oh-my-zsh $HOME/.oh-my-zsh
+    # git clone oh-my-zsh
+    git clone https://github.com/robbyrussell/oh-my-zsh $HOME/.oh-my-zsh
 else
-  cd $HOME/.oh-my-zsh
-  git pull origin master
-  cd -
+    pushd $HOME/.oh-my-zsh
+    git pull origin master
+    popd -
 fi
 
-if ! [[ -d $HOME/GitHub/mine/zsh-theme ]]; then
+if ! [[ -d $HOME/GitHub/mine/scripts/zsh-theme ]]; then
 # Get my self-made zsh-themes
-  git clone https://github.com/fusion809/zsh-theme $HOME/GitHub/mine/zsh-theme
-  cp -a $HOME/GitHub/mine/zsh-theme/*.zsh-theme $HOME/.oh-my-zsh/themes/
+    git clone https://github.com/fusion809/zsh-theme $HOME/GitHub/mine/scripts/zsh-theme
+    cp -a $HOME/GitHub/mine/zsh-theme/*.zsh-theme $HOME/.oh-my-zsh/themes/
 else
-  cd $HOME/GitHub/mine/zsh-theme
-  git pull origin master
-  cd -
-  cp -a $HOME/GitHub/mine/zsh-theme/*.zsh-theme $HOME/.oh-my-zsh/themes/
+    pushd $HOME/GitHub/mine/scripts/zsh-theme
+    git pull origin master
+    popd -
+    cp -a $HOME/GitHub/mine/zsh-theme/*.zsh-theme $HOME/.oh-my-zsh/themes/
 fi
 
 if ! [[ -d $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting ]]; then
-  # Get zsh-syntax-highlighting
-  git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
+    # Get zsh-syntax-highlighting
+    git clone https://github.com/zsh-users/zsh-syntax-highlighting $HOME/.oh-my-zsh/plugins/zsh-syntax-highlighting
 fi
 
 if ! [[ -d $HOME/.oh-my-zsh/plugins/zsh-history-substring-search ]]; then
-  git clone https://github.com/zsh-users/zsh-history-substring-search $HOME/.oh-my-zsh/plugins/zsh-history-substring-search
+    git clone https://github.com/zsh-users/zsh-history-substring-search $HOME/.oh-my-zsh/plugins/zsh-history-substring-search
 fi
 
 # Change default login shell to Zsh
